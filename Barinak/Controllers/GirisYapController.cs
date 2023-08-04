5710e06fae2d9a9fdcf8322e9bc6dd09e93b45b9
onuraydinoglu <onur.aydinoglu.98@gmail.com>
2023-08-04 21:59:53 +0300

    Admin ve Kullanıcı

--- Barinak/Controllers/GirisYapController.cs
+++ Barinak/Controllers/GirisYapController.cs
@@ -14,27 +14,22 @@ namespace Barinak.Controllers
         }
 
         public IActionResult GirisYap(UyeOl uye)
-        {
-            var usr = k.Uyeler.Where(x => x.Mail == uye.Mail && x.Sifre == uye.Sifre).FirstOrDefault();
-            if (usr is not null)
-            {
-                var cookopt = new CookieOptions
-                {
-                    Expires = DateTime.Now.AddSeconds(1000)
-                };
-                HttpContext.Session.SetString("SessionMail", uye.Mail);
-                HttpContext.Response.Cookies.Append("UserAd", uye.Sifre, cookopt);
-                return RedirectToAction("~/Home");
-                /*  Nesneyi sesisoda saklama
-                var struser = JsonConvert.SerializeObject(usr);
-                HttpContext.Session.SetString("SesUser", struser);
+         {
+             var usr = k.Uyeler.Where(x => x.Mail == uye.Mail && x.Sifre == uye.Sifre).FirstOrDefault();
+             if (usr is not null)
+             {
+                 var cookopt = new CookieOptions
+                 {
+                     Expires = DateTime.Now.AddSeconds(1000)
+                 };
+                 HttpContext.Session.SetString("SessionMail", uye.Mail);
+                 HttpContext.Response.Cookies.Append("UserAd", usr.Ad , cookopt);
+                 return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+             }
+             TempData["msj"] = "Hatalı Giriş Yapıldı";
+             return View("Index");
+         }
 
-                var st = HttpContext.Session.GetString("SesUser");
-                var usr1 = JsonConvert.DeserializeObject<User>(st);
-                */
-            }
-            TempData["msj"] = "Hatalı Giriş Yapıldı";
-            return View("Index");
-        }
+
     }
 }
